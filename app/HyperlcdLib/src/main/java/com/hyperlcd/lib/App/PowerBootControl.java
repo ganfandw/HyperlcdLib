b31579d82c10ba01cb9f183a4f2a290201da5b36
ning100zz <1808937282@qq.com>
2023-02-02 13:42:17 +0800

    v1.0.6 完善开机自启动功能

--- app/HyperlcdLib/src/main/java/com/hyperlcd/lib/App/PowerBootControl.java
+++ app/HyperlcdLib/src/main/java/com/hyperlcd/lib/App/PowerBootControl.java
@@ -12,7 +12,7 @@ import com.hyperlcd.lib.WifiManager.WifiControl;
  * 接口功能：
  * </br>
  * <ol>
- * <li>{@link WifiControl#openWifi()} 打开Wifi</li>
+ * <li>{@link PowerBootControl#setPowerBootState(boolean)} true设置开机自启，false取消开机自启</li>
  * <li>{@link WifiControl#closeWifi()} 关闭Wifi</li>
  * </ol>
  */
@@ -20,6 +20,7 @@ public class PowerBootControl {
 
     private static PowerBootControl mInstance = null;
     private Context mContext;
+    private String mPackageName;
     private static SharedPreferences mSharedPreferences;
     private static SharedPreferences.Editor mEditor;
     PowerBootControl(Context context){
@@ -29,6 +30,7 @@ public class PowerBootControl {
     /**
      * 获取开机启动控制器类控制单例
      * @param context 上下文对象
+     * @param packageName 需要启动应用的包名
      * @return PowerBootControl 单例
      */
     public static PowerBootControl getInstance(Context context) {
@@ -44,13 +46,25 @@ public class PowerBootControl {
     }
 
     /**
-     * <p>设置软件开机自启状态</p>
+     * <p>设置软件开机自启</p>
      * </br>
-     * @Param flag boolean型变量 true为开机自启，false为取消开机自启
+     * @Param packageName 需要自启动的应用包名
      * @return void
      */
-    public void setPowerBootState(boolean flag){
-        mEditor.putBoolean("POWER_BOOT",flag);
+    public void setPowerBootStart(String packageName){
+        mEditor.putBoolean("POWER_BOOT",true);
+        mEditor.putString("PACKAGE_NAME",packageName);
         mEditor.commit();
     }
+
+    /**
+     * <p>取消软件开机自启</p>
+     * </br>
+     * @return void
+     */
+    public void setPowerBootClose(){
+        mEditor.putBoolean("POWER_BOOT",false);
+        mEditor.commit();
+    }
+
 }
--- app/HyperlcdLib/src/main/java/com/hyperlcd/lib/Utils/BootReceiver.java
+++ app/HyperlcdLib/src/main/java/com/hyperlcd/lib/Utils/BootReceiver.java
@@ -14,12 +14,19 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {     // boot
-
             mSharedPreferences = context.getSharedPreferences("hyperlcdLibShared",Context.MODE_PRIVATE);
-            Log.e("power boot","开机自启动"+mSharedPreferences.getBoolean("POWER_BOOT",false));
-//            Intent intent2 = new Intent(context, LoginActivity.class);
-//            intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            context.startActivity(intent2);
+            if (mSharedPreferences!=null){
+                if (mSharedPreferences.getBoolean("POWER_BOOT",false)){
+                    String packageName = mSharedPreferences.getString("PACKAGE_NAME","");
+                    //如果自启动APP，参数为需要自动启动的应用包名
+                    Intent mIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+                    //这句话必须加上才能开机自动运行app的界面
+                    mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    //自启动Activity
+                    context.startActivity(mIntent);
+                }
+                Log.e("POWER_BOOT","开机自启动"+mSharedPreferences.getBoolean("POWER_BOOT",false));
+            }
         }
         if (intent.getAction().equals("android.intent.action.ACTION_SHUTDOWN")){
             Log.e("shutdown","关机了");
